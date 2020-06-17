452d39eb3f55d3ebed13976abf0564d19e50fe0d
Tomas Perez <tomasitopg6@gmail.com>
2020-06-17 01:33:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,6 @@ def speak_to_grandma(phrase)
   end
 end
 
-speak_to_grandma("HELLO THERE")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
