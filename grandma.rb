bc692c1f1780e3796e52cbb48440ecd83fbb3e7b
Tomas Perez <tomasitopg6@gmail.com>
2020-06-17 02:05:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,6 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else
-    return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
