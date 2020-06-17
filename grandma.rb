c422b90733ab45b16c9d73abfa09dd0d54176d0a
Tomas Perez <tomasitopg6@gmail.com>
2020-06-17 01:51:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase == phrase.downcase
+  elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif phrase == phrase.downcase
+  elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "NO, NOT SINCE 1938!"
